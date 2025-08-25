1da3f10fadaa400c5092f44d2fbcf96fd813ce78
Christian Lopes <christianx111.ls@gmail.com>
2025-08-25 00:25:52 -0300

    condiçoes adicionadas

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
-function Calcular(tipo, valor){
-    console.log(tipo,valor)
+function Calcular(tipo, valor) {
+    if (tipo == 'acao') {
+
+        if (valor == 'c') {
+            // limpar visor
+            document.getElementById('resultado').value = '';
+        }
+
+        if (valor == '+' || valor == "-" || valor == "*" || valor == '/' || valor == '.') {
+            let campo = document.getElementById('resultado').value;
+            let ultimoChar = campo.slice(-1); // pega o último caractere digitado
+
+            // só permite operador se:
+            // 1. já existir algo no campo
+            // 2. o último caractere NÃO for outro operador
+            if (campo.length > 0 && !['+', '-', '*', '/', '.'].includes(ultimoChar)) {
+                document.getElementById('resultado').value += valor;
+            }
+        }
+
+        // converter resultado
+        if (valor == '=') {
+            var valor_campo = eval(document.getElementById('resultado').value);
+            document.getElementById('resultado').value = valor_campo;
+        }
+
+    } else if (tipo == 'valor') {
+        document.getElementById('resultado').value += valor;
+    }
 }
